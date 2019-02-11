050a881ebc3d768aff7ea63156c458bfa439c92a
Taylor Short <treddshort@gmail.com>
2019-02-11 09:09:04 -0800

    added ejs functionality

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -5,19 +5,10 @@ const base = "http://localhost:3000/";
 describe("routes : static", () => {
 
   describe("GET /", () => {
-    it("should return status code 200", (done) => {
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
-        done();
-      });
-    });
-  });
-
-  describe("GET /marco", () => {
-    it("should return status code 200 and have polo in the body", (done) => {
-      request.get(`${base}marco`, (err, res, body) => {
-        expect(res.statusCode).toBe(200);
-        expect(body).toContain('polo');
+        expect(body).toContain("Welcome to Bloccit");
         done();
       });
     });
